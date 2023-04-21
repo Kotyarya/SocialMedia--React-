e363fcfa0b47e76d4dc8c895d3d1d98ed866e993
Kotyarya <sunguuner500@gmail.com>
2023-04-21 16:24:32 +0200

    changed

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -1,76 +1,18 @@
 import style from "./Users.module.css"
+import axios from "axios";
+import userPhoto from "../../assets/img/userPhoto.png"
 
 const Users = (props) => {
 
-
     if (props.users.length === 0) {
-        props.setUsers([
-                {
-                    id: 1,
-                    fullName: "User1",
-                    followed: false,
-                    status: "Status1",
-                    location: {city: "City1", country: "Country1"}
-                },
-                {
-                    id: 2,
-                    fullName: "User2",
-                    followed: true,
-                    status: "Status2",
-                    location: {city: "City2", country: "Country2"}
-                },
-                {
-                    id: 3,
-                    fullName: "User3",
-                    followed: false,
-                    status: "Status3",
-                    location: {city: "City3", country: "Country3"}
-                },
-                {
-                    id: 4,
-                    fullName: "User4",
-                    followed: true,
-                    status: "Status4",
-                    location: {city: "City4", country: "Country4"}
-                },
-                {
-                    id: 5,
-                    fullName: "User5",
-                    followed: true,
-                    status: "Status5",
-                    location: {city: "City5", country: "Country5"}
-                },
-                {
-                    id: 6,
-                    fullName: "User6",
-                    followed: false,
-                    status: "Status6",
-                    location: {city: "City6", country: "Country6"}
-                },
-                {
-                    id: 7,
-                    fullName: "User7",
-                    followed: true,
-                    status: "Status7",
-                    location: {city: "City7", country: "Country7"}
-                },
-                {
-                    id: 8,
-                    fullName: "User8",
-                    followed: false,
-                    status: "Status8",
-                    location: {city: "City8", country: "Country8"}
-                },
-                {
-                    id: 9,
-                    fullName: "User9",
-                    followed: true,
-                    status: "Status9",
-                    location: {city: "City9", country: "Country9"}
-                },
-            ])
-    }
 
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+            .then(response => {
+                props.setUsers(response.data.items)
+            })
+
+
+    }
 
     return (
         <>
@@ -78,13 +20,14 @@ const Users = (props) => {
                 {props.users.map(item => {
                     return (
                         <div key={item.id} className={style.block}>
+                            <img className={style.avatar} src={
+                                item.photos.small === null
+                                    ? userPhoto
+                                    : `${item.photos.small}`
+                            } alt=""/>
                             <div>
-                                <p>{item.fullName}</p>
+                                <p>{item.name}</p>
                                 <p>{item.status}</p>
-                            </div>
-                            <div>
-                                <p>{item.location.city}</p>
-                                <p>{item.location.country}</p>
                             </div>
                             {
                                 item.followed
--- src/Redux/Reducers/usersReducer.js
+++ src/Redux/Reducers/usersReducer.js
@@ -5,15 +5,6 @@ const setUsers = "setUsers"
 
 const initialState = {
     users: [
-        { id: 1, fullName:"User1", followed: false, status : "Status1", location: {city : "City1" , country : "Country1"}},
-        { id: 2, fullName:"User2", followed: true, status : "Status2", location: {city : "City2" , country : "Country2"}},
-        { id: 3, fullName:"User3", followed: false, status : "Status3", location: {city : "City3" , country : "Country3"}},
-        { id: 4, fullName:"User4", followed: true, status : "Status4", location: {city : "City4" , country : "Country4"}},
-        { id: 5, fullName:"User5", followed: true, status : "Status5", location: {city : "City5" , country : "Country5"}},
-        { id: 6, fullName:"User6", followed: false, status : "Status6", location: {city : "City6" , country : "Country6"}},
-        { id: 7, fullName:"User7", followed: true, status : "Status7", location: {city : "City7" , country : "Country7"}},
-        { id: 8, fullName:"User8", followed: false, status : "Status8", location: {city : "City8" , country : "Country8"}},
-        { id: 9, fullName:"User9", followed: true, status : "Status9", location: {city : "City9" , country : "Country9"}},
     ]
 }
 
@@ -37,7 +28,7 @@ const usersReducer = (state = initialState,action) => {
             })
             return stateCopy
         case setUsers:
-            stateCopy.users = [stateCopy.users, ...action.users]
+            stateCopy.users = [...action.users]
             return stateCopy
         default:
             return state
